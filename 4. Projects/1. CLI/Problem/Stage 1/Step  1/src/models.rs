e5aff62be0302b2ccd42e4645f19d273132c0cb6
muaazsaleem <5851294+muaazsaleem@users.noreply.github.com>
2025-10-25 21:52:15 +0200

    Improve unit tests

--- 4. Projects/1. CLI/Problem/Stage 1/Step  1/src/models.rs
+++ 4. Projects/1. CLI/Problem/Stage 1/Step  1/src/models.rs
@@ -1,5 +1,6 @@
 use std::collections::HashMap;
 
+#[derive(Debug, PartialEq)]
 pub enum Status {
   Open,
   InProgress,
@@ -7,6 +8,7 @@ pub enum Status {
   Closed,
 }
 
+#[derive(Debug, PartialEq)]
 pub struct Epic {
   pub name: String,
   pub description: String,
@@ -27,6 +29,7 @@ impl Epic {
     }
 }
 
+#[derive(Debug, PartialEq)]
 pub struct Story {
   pub name: String,
   pub description: String,
@@ -44,9 +47,80 @@ impl Story {
     }
 }
 
+#[derive(Debug, PartialEq)]
 pub struct DBState {
     // This struct represents the entire db state which includes the last_item_id, epics, and stories
     pub last_item_id: u32,
     pub epics: HashMap<u32, Epic>,
-    pub stories: HashMap<u32, Epic>,
+    pub stories: HashMap<u32, Story>,
+}
+
+impl Default for DBState {
+  fn default() -> Self {
+      Self {
+        last_item_id: 0,
+        epics: HashMap::new(),
+        stories: HashMap::new(),
+      }
+  }
+}
+
+
+#[cfg(test)]
+mod tests {
+ use super::*;
+
+ #[test]
+ fn test_epic_creation() {
+   let epic = Epic::new("New Epic".to_string(), "Epic description".to_string());
+   
+   let expected_epic = Epic {
+      name: "New Epic".to_string(),
+      description: "Epic description".to_string(),
+      status: Status::Open,
+      stories: vec![]
+   };
+   
+   assert_eq!(epic, expected_epic);
+ }
+
+ #[test]
+ fn test_story_creation() {
+    let story = Story::new("New Story".to_string(), "Story description".to_string());
+
+    let expected_story = Story {
+      name: "New Story".to_string(),
+      description: "Story description".to_string(),
+      status: Status::Open,
+    };
+
+    assert_eq!(story, expected_story)
+ }
+
+ #[test]
+ fn test_dbstate_default_properties() {
+    let db_state = DBState::default();
+
+    assert_eq!(db_state.last_item_id, 0);
+    assert!(db_state.epics.is_empty());
+    assert!(db_state.stories.is_empty());
+ }
+
+ #[test]
+ fn test_add_epic_to_db() {
+   let mut db_state = DBState::default();
+   let epic = Epic::new("New Epic".to_string(), "Epic Description".to_string());
+   let epic_id = 1;
+   
+   db_state.epics.insert(epic_id, epic);
+   db_state.last_item_id = epic_id;
+
+   let retrieved_epic = db_state.epics.get(&epic_id).unwrap();
+   
+   assert_eq!(retrieved_epic.name, "New Epic".to_string());
+   assert_eq!(db_state.last_item_id, 1);
+   assert_eq!(db_state.stories.len(), 0);
+   
+ }
+
 }
