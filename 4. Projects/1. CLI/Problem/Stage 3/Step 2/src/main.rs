2f71309a0ba413f00f64827de1d5b3b31c7d962c
Muaaz Saleem <muaazsaleem@outlook.com>
2025-11-22 21:21:24 +0100

    First pass at main.rs

--- 4. Projects/1. CLI/Problem/Stage 3/Step 2/src/main.rs
+++ 4. Projects/1. CLI/Problem/Stage 3/Step 2/src/main.rs
@@ -14,16 +14,31 @@ mod navigator;
 use navigator::*;
 
 fn main() {
-    // TODO: create database and navigator
-    
+    // create database and navigator
+    let db = JiraDatabase::new("data/db.json".to_string());
+    let mut nav = Navigator::new(Rc::new(db));
+
     loop {
         clearscreen::clear().unwrap();
 
-        // TODO: implement the following functionality:
         // 1. get current page from navigator. If there is no current page exit the loop.
-        // 2. render page
-        // 3. get user input
-        // 4. pass input to page's input handler
-        // 5. if the page's input handler returns an action let the navigator process the action
+
+        if let Some(page) = nav.get_current_page() {
+            // 2. render page
+            if !page.draw_page().is_ok() {
+                break;
+            }
+            // 3. get user input
+            let input = get_user_input();
+            // 4. pass input to page's input handler
+            if let Ok(Some(action)) = page.handle_input(&input) {
+                // 5. if the page's input handler returns an action let the navigator process the action
+                nav.handle_action(action);
+            } else {
+                break;
+            }
+        } else {
+            break;
+        }
     }
 }
